211d79e4fdfc8737d0a586997a090955a4b437b7
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2020-10-15 13:09:20 +0200

    Add files via upload

---
+++ 20201015-1105_footer_spinner_generico.js
@@ -0,0 +1,43 @@
+document.getElementById("footer_spinner_generico").innerHTML=
+' <footer class="position-fixed bottom-0 left-0 rounded bg_giallobsn-0_85"> '+
+'  '+
+'  '+
+' <span id="spinner" class="fa fa-spinner fa-pulse"></span> '+
+
+'  '+
+' </footer> '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+
+''
+;
---
+++ 20201015-1245_footer_sdudu_generico.js
@@ -0,0 +1,16 @@
+document.getElementById("footer_sdudu_generico").innerHTML='<footer class="position-fixed bottom-0 left-0 rounded bg_giallobsn-0_85">   <span id="spinner" class="fa fa-spinner fa-pulse"></span>  <a href="#fine" style="text-decoration: none;">  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">  <path fill-rule="evenodd" fill="maroon" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>  </svg> </a>  <a href="#" style="text-decoration: none;">  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">  <path fill-rule="evenodd" fill="maroon" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>  </svg> </a>  <div class="btn-group dropup" >  <button type="button"   class="btn btn-secondary rounded bg_giallobsn"   data-toggle="dropdown"   aria-expanded="false"   style="  background-color:rgba(255,170,0,0.85);  color:maroon;   border:none; box-shadow:none;">   <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chat-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">  <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h2.5a2 2 0 0 1 1.6.8L8 14.333 9.9 11.8a2 2 0 0 1 1.6-.8H14a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>  </svg>   </button>  <ul class="dropdown-menu shadow p-3 mb-1 bg_giallobsn">   '+
+'   '+
+'   '+
+'<li><a href="#">lista uno</a></li> '+
+'   '+
+
+'<li><a href="#fine">lista due</a></li> '+
+'   '+
+
+'<li><a href="#">lista tre</a></li> '+
+'   '+
+
+'<li><a href="#fine">lista quattro</a></li> '+
+'   '+
+
+'</ul> </div> </footer> '+ '' ; 
