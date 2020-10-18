f2e6c6edc7af592ecfa2c459e7ef2f8813f928a6
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2020-10-18 10:54:09 +0200

    Add files via upload

---
+++ bs500a2-CSS.js
@@ -0,0 +1,1 @@
+document.getElementById("footer_head_bs5_css").innerHTML='<link rel="stylesheet" '+'href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">'+'';
